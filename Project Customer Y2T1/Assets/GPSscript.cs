d0d80ceed0785cfaed50a7c1728d1f8867f9d9e4
roanweller <roanweller10@gmail.com>
2022-09-19 15:54:25 +0200

    Duplicated the demo scene and renamed it to GPS thing and added the map

---
+++ Project Customer Y2T1/Assets/GPSscript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSscript : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void LateUpdate ()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
