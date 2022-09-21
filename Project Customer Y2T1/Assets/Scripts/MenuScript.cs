4aad1d5d05878f33c5d5ba61589dac95b4880681
taix97 <44111519+taix97@users.noreply.github.com>
2022-09-21 18:57:02 +0300

    Main menu buttons

--- Project Customer Y2T1/Assets/Scripts/MenuScript.cs
+++ Project Customer Y2T1/Assets/Scripts/MenuScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,51 +11,56 @@ public class MenuScript : MonoBehaviour
     public GameObject optionText;
     public GameObject manualText;
     public GameObject creditsText;
-public void OptionMenu(){
-mainMenu.SetActive(false);
-mainText.SetActive(false);
-creditsMenu.SetActive(false);
-creditsText.SetActive(false);
-manualMenu.SetActive(false);
-manualText.SetActive(false);
-optionMenu.SetActive(true);
-optionText.SetActive(true);
-}
+    public void OptionMenu()
+    {
+        mainMenu.SetActive(false);
+        mainText.SetActive(false);
+        creditsMenu.SetActive(false);
+        creditsText.SetActive(false);
+        manualMenu.SetActive(false);
+        manualText.SetActive(false);
+        optionMenu.SetActive(true);
+        optionText.SetActive(true);
+    }
 
-public void ManualMenu(){
-mainMenu.SetActive(false);
-mainText.SetActive(false);
-creditsMenu.SetActive(false);
-creditsText.SetActive(false);
-manualMenu.SetActive(true);
-manualText.SetActive(true);
-optionMenu.SetActive(false);
-optionText.SetActive(false);
-}
+    public void ManualMenu()
+    {
+        mainMenu.SetActive(false);
+        mainText.SetActive(false);
+        creditsMenu.SetActive(false);
+        creditsText.SetActive(false);
+        manualMenu.SetActive(true);
+        manualText.SetActive(true);
+        optionMenu.SetActive(false);
+        optionText.SetActive(false);
+    }
 
-public void CreditsMenu(){
-mainMenu.SetActive(false);
-mainText.SetActive(false);
-creditsMenu.SetActive(true);
-creditsText.SetActive(true);
-manualMenu.SetActive(false);
-manualText.SetActive(false);
-optionMenu.SetActive(false);
-optionText.SetActive(false);
-}
+    public void CreditsMenu()
+    {
+        mainMenu.SetActive(false);
+        mainText.SetActive(false);
+        creditsMenu.SetActive(true);
+        creditsText.SetActive(true);
+        manualMenu.SetActive(false);
+        manualText.SetActive(false);
+        optionMenu.SetActive(false);
+        optionText.SetActive(false);
+    }
 
-public void MainMenu(){
-mainMenu.SetActive(true);
-mainText.SetActive(true);
-creditsMenu.SetActive(false);
-creditsText.SetActive(false);
-manualMenu.SetActive(false);
-manualText.SetActive(false);
-optionMenu.SetActive(false);
-optionText.SetActive(false);
-}
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        mainText.SetActive(true);
+        creditsMenu.SetActive(false);
+        creditsText.SetActive(false);
+        manualMenu.SetActive(false);
+        manualText.SetActive(false);
+        optionMenu.SetActive(false);
+        optionText.SetActive(false);
+    }
 
-public void StartButton(){
-SceneManager.LoadScene(1);
-}
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
