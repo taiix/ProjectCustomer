ad119800df3d64d24e83f6ddc8eb32c99e27c174
roanweller <roanweller10@gmail.com>
2022-09-21 17:00:28 +0200

    Made main menu

--- Project Customer Y2T1/Assets/MenuScript.cs
+++ Project Customer Y2T1/Assets/MenuScript.cs
@@ -9,26 +9,52 @@ public class MenuScript : MonoBehaviour
     public GameObject optionMenu;
     public GameObject manualMenu;
     public GameObject creditsMenu;
+    public GameObject mainText;
+    public GameObject optionText;
+    public GameObject manualText;
+    public GameObject creditsText;
 public void OptionMenu(){
 mainMenu.SetActive(false);
+mainText.SetActive(false);
+creditsMenu.SetActive(false);
+creditsText.SetActive(false);
+manualMenu.SetActive(false);
+manualText.SetActive(false);
 optionMenu.SetActive(true);
+optionText.SetActive(true);
 }
 
 public void ManualMenu(){
 mainMenu.SetActive(false);
+mainText.SetActive(false);
+creditsMenu.SetActive(false);
+creditsText.SetActive(false);
 manualMenu.SetActive(true);
+manualText.SetActive(true);
+optionMenu.SetActive(false);
+optionText.SetActive(false);
 }
 
 public void CreditsMenu(){
 mainMenu.SetActive(false);
+mainText.SetActive(false);
 creditsMenu.SetActive(true);
+creditsText.SetActive(true);
+manualMenu.SetActive(false);
+manualText.SetActive(false);
+optionMenu.SetActive(false);
+optionText.SetActive(false);
 }
 
 public void MainMenu(){
 mainMenu.SetActive(true);
-optionMenu.SetActive(false);
-manualMenu.SetActive(false);
+mainText.SetActive(true);
 creditsMenu.SetActive(false);
+creditsText.SetActive(false);
+manualMenu.SetActive(false);
+manualText.SetActive(false);
+optionMenu.SetActive(false);
+optionText.SetActive(false);
 }
 
 public void StartButton(){
